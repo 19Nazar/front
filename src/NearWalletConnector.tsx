be8bda6d873a6af926e000d902b176f05c02eb00
19Nazar <vballiev@gmail.com>
2024-01-31 15:50:31 +0200

    add: modal.tsx, modall.css

--- src/NearWalletConnector.tsx
+++ src/NearWalletConnector.tsx
@@ -4,28 +4,41 @@ import { setupModal, WalletSelectorModal} from "@near-wallet-selector/modal-ui";
 import { setupNearWallet } from "@near-wallet-selector/near-wallet";
 import "@near-wallet-selector/modal-ui/styles.css"
 import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
-import { setupWalletConnect } from "@near-wallet-selector/wallet-connect";
-import { setupMintbaseWallet } from "@near-wallet-selector/mintbase-wallet"; 
-import { Buffer as BufferPolyfill } from 'buffer'
-declare var Buffer: typeof BufferPolyfill;
+// import { setupWalletConnect } from "@near-wallet-selector/wallet-connect";
+// import { setupMintbaseWallet } from "@near-wallet-selector/mintbase-wallet"; 
+import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
+import { setupMathWallet } from "@near-wallet-selector/math-wallet";
+import { setupNarwallets } from "@near-wallet-selector/narwallets";
+import CustomModal from './modal';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
 
 export const NearWalletConnector = () => {
+  
   const [selector, setSelector] = useState<WalletSelector | null>(null);
   const [modal, setModal] = useState<WalletSelectorModal | null>(null);
   const [accounts, setAccounts] = useState<Array<AccountState>>([]);
-  const mintbaseWallet =  setupMintbaseWallet({
-    walletUrl: 'https://testnet.wallet.mintbase.xyz',
-    callbackUrl: 'http://localhost:5173/',
-    deprecated: false,
-    contractId: "test.testnet"
-  });
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [nick, setNick] = useState("");  
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isGroupCreated, setIsGroupCreated] = useState(false);
+  // const mintbaseWallet =  setupMintbaseWallet({
+  //   walletUrl: 'https://testnet.wallet.mintbase.xyz',
+  //   callbackUrl: 'http://localhost:5173/',
+  //   deprecated: false,
+  //   contractId: "test.testnet"
+  // });
 
   useEffect(() => {
     const initializeWallet = async () => {
       const selector = await setupWalletSelector({
         network: "testnet",
-        modules: [setupNearWallet(),
+        modules: [setupNearWallet({walletUrl: "https://wallet.testnet.near.org",}),
                   setupMyNearWallet(),
+                  setupMeteorWallet(),
+                  setupMathWallet(),
+                  setupNarwallets(),
                   // setupWalletConnect({
                   //   projectId: "c4f79cc...",
                   //   metadata: {
@@ -36,56 +49,174 @@ export const NearWalletConnector = () => {
                   //   },
                   //   chainId: "near:testnet",
                   // }),
-                  mintbaseWallet,
+                  // mintbaseWallet,
                   ],
       });
       const modal = setupModal(selector, {
-        contractId: "test.testnet",
+        contractId: "dev-1706442202297-21944990165378",
       });
-
       setSelector(selector);
       setModal(modal);
       
     };
-
     initializeWallet();
   }, []);
 
-  // const signIn = async () => {
-  //   if (!selector) return; // Проверка на наличие инициализированного selector
-  //   const wallet = await selector.wallet("my-near-wallet");
-  //   if (wallet) {
-  //     const account = await wallet.signIn({ contractId: "test.testnet",});
-  //     // Выполните необходимые действия после входа в кошелек
-  //   }
-  // };
-  
+
   const signOut = async () => {
     if (!selector) return; // Проверка на наличие инициализированного selector
     const wallet = await selector.wallet("my-near-wallet");
     if (wallet) {
       await wallet.signOut();
-    }
-  };
-  
-  const getAccounts = async () => {
-    if (!selector) return; // Проверка на наличие инициализированного selector
-    const wallet = await selector.wallet("my-near-wallet");
-    if (wallet) {
-      const accounts = await wallet.getAccounts();
-      console.log(accounts); // [{ accountId: "test.testnet" }]
+      window.location.reload();
     }
   };
 
+  useEffect(() => {
+    if (selector) {
+      setIsSignedIn(selector.isSignedIn());
+      getAccounts();
+    }
+  }, [selector]);
+
+  
+  
+  const getAccounts = async () => {
+    if (!selector) return;
+    const wallet = await selector.wallet("my-near-wallet");
+    if (wallet) {
+      const account = await wallet.getAccounts();
+      const accountId = account[0].accountId;
+      setNick(accountId);
+    }
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleGroupCreate = () => {
+    // logic for create group
+    setIsGroupCreated(true);
+  };
+ 
+
   return (
     <div>
-      <h1>NEAR Wallet Authorization</h1>
-      {/* <button onClick={signIn}>Sign in</button> */}
-      <button onClick={signOut}>Sign Out</button>
-      <button onClick={getAccounts}>Get Accounts</button>
-      {modal && (
-        <button onClick={() => modal.show()}>Authorize with NEAR Wallet</button>
+      <div className="Header">
+      {!isSignedIn && modal && (
+        <div>
+          <button onClick={() => modal.show()}>Authorize with NEAR Wallet</button>
+        </div>
       )}
+      {isSignedIn && !isGroupCreated && (
+        <div>
+          <button onClick={openModal}>Create Group</button>
+          <CustomModal isOpen={isModalOpen} onClose={closeModal} onGroupCreate={handleGroupCreate} />
+        </div>
+      )}
+      {isSignedIn && (
+        <div>
+          <button onClick={signOut}>Sign Out {nick}</button>  
+        </div>
+      )}
+      </div>
+      <div className="line"></div>
+      <div className='Tabs'>
+        {isSignedIn && isGroupCreated && (
+          <>
+          <Tabs>
+            <TabList>
+              <Tab>Add users to group</Tab>
+              <Tab>Set permission to the user</Tab>
+              <Tab>Delete user from group</Tab>
+              <Tab>Create event</Tab>
+              <Tab>Read event</Tab>
+              <Tab>View groups</Tab>
+            </TabList>
+            <div>
+              <TabPanel>
+                <div className="start_module">
+                    <h1>You can add users to the group</h1>
+                </div>
+                <div className="content_TabPanel">
+                  <div>
+                    <h3>Insert user name:</h3>
+                  </div>
+                  <form>
+                    <input className="inputt" placeholder="Name"/>
+                  </form>
+                  <button className="button_in_TabPanel">Add</button>
+                </div>
+              </TabPanel>
+              <TabPanel>
+                <div className="start_module">
+                    <h1>You can set permission to the user</h1>
+                </div>
+                <div className="content_TabPanel">
+                  <div>
+                    <h3>Insert user name:</h3>
+                  </div>
+                  <form>
+                    <input className="inputt" placeholder="Name" />
+                  </form>
+                  <div>
+                    <h3>Set permission:</h3>
+                  </div>
+                  <form>
+                    <input className="inputt" placeholder="Permission" />
+                  </form>
+                  <button className="button_in_TabPanel">Add</button>
+                </div>
+              </TabPanel>
+              <TabPanel>
+                <div className="start_module">
+                    <h1>You can delete user from group</h1>
+                </div>
+                <div className="content_TabPanel">
+                  <div>
+                    <h3>Insert user name:</h3>
+                  </div>
+                  <form>
+                    <input className="inputt" placeholder="Name" />
+                  </form>
+                  <button className="button_in_TabPanel">Delete</button>
+                </div>
+              </TabPanel>
+              <TabPanel>
+                <div className="start_module">
+                    <h1>You can create event</h1>
+                </div>
+                <div className="content_TabPanel">
+                  <div>
+                    <h3>Name of event:</h3>
+                  </div>
+                  <form>
+                    <input className="inputt" placeholder="Name" />
+                  </form>
+                  <button className="button_in_TabPanel">Create</button>
+                </div>
+              </TabPanel>
+              <TabPanel>
+                <div className="start_module">
+                    <h1>You can read event</h1>
+                </div>
+              </TabPanel>
+              <TabPanel>
+                <div className="start_module">
+                  <h1>You can view groups</h1>
+                </div>
+              </TabPanel>
+            </div>
+
+          </Tabs>
+        </>
+      )}
+      </div>
     </div>
   );
 };
---
+++ src/modal.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import './modal.css';
+
+interface CustomModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onGroupCreate?: () => void; 
+}
+
+const CustomModal: React.FC<CustomModalProps> = ({ isOpen, onClose, onGroupCreate }) => {
+  const [formCount, setFormCount] = useState(1);
+  const [groupName, setGroupName] = useState('');
+
+  const handleAddForm = () => {
+    setFormCount((prevCount) => prevCount + 1);
+  };
+
+  const handleGroupNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGroupName(e.target.value);
+  };
+
+  const handleCreate = () => {
+    // Logic for tap in button create
+    console.log('Creating group:', groupName);
+    onClose();
+    onGroupCreate && onGroupCreate();
+  };
+
+  const customStyles = {
+    content: {
+      width: '60%',
+      height: '60%',
+      margin: 'auto',
+    },
+  };
+
+  return (
+    <div className="main">
+      <Modal isOpen={isOpen} onRequestClose={onClose} style={customStyles}>
+        <div className="header">
+          <h2>Settings group</h2>
+        </div>
+        <div>
+          <h3>Group name (this field is mandatory)</h3>
+          <form>
+            <input
+              className="inputt"
+              placeholder="Name"
+              value={groupName}
+              onChange={handleGroupNameChange}
+            />
+          </form>
+        </div>
+        <div>
+          <h3>
+            Set custom attributes for your token, indicating it's properties.
+            These will become filters on your contract page.
+          </h3>
+        </div>
+        <div>
+          {[...Array(formCount)].map((_, index) => (
+            <form key={index}>
+              <input className="inputt" placeholder="Name" />
+              <input className="inputt" placeholder="Value" />
+            </form>
+          ))}
+        </div>
+        <div className="add-form-button">
+          <button onClick={handleAddForm}>+</button>
+        </div>
+        <div className="buttons-container">
+          <button className="create-button" onClick={handleCreate} disabled={!groupName.trim()}>
+            Create
+          </button>
+          <button className="close-button" onClick={onClose}>
+            Close
+          </button>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default CustomModal;
